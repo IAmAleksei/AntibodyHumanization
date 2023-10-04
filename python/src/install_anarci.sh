165a18b67e49b35e062c2f60da5f44803f20fe23
alex-shishkin <alex-shishkin@yandex-team.ru>
2023-10-04 14:26:15 +0300

    Freeze hmmer version

--- python/src/install_anarci.sh
+++ python/src/install_anarci.sh
@@ -1,4 +1,4 @@
-curl http://eddylab.org/software/hmmer/hmmer.tar.gz > hmmer.tar.gz
+curl http://eddylab.org/software/hmmer/hmmer-3.3.2.tar.gz > hmmer.tar.gz
 tar xf hmmer.tar.gz
 cd hmmer-3.3.2
 ./configure
