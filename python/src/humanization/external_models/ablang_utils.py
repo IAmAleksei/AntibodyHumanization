1ed68a6c7b4587b5418bb809f2e58bae6aa8b2df
alex-shishkin <alex-shishkin@yandex-team.ru>
2024-04-07 18:40:19 +0300

    Add attention grouping calculation

--- python/src/humanization/external_models/ablang_utils.py
+++ python/src/humanization/external_models/ablang_utils.py
@@ -1,7 +1,21 @@
 import ablang2
+import numpy as np
+import torch
 
 ablang = ablang2.pretrained(model_to_use='ablang2-paired', random_init=False, ncpu=1, device='cpu')
 
 
 def get_ablang_embedding(seq: str):
     return ablang([seq, ''], mode='seqcoding')
+
+
+def get_attentions(vh: str):
+    length = len(vh)
+    tokenized_seqs = ablang.tokenizer([f"<{vh}>|"], pad=True, w_extra_tkns=False)
+    with torch.no_grad():
+        attn_weights = ablang.AbLang.forward(tokenized_seqs, return_attn_weights=True)[-1][0]
+        result = []
+        for pos in range(length):
+            token = tokenized_seqs[0][pos + 1]  # Ignoring first token '<'
+            result.append(attn_weights[token - 1][pos + 1][1:length + 1])
+        return np.array(result)
---
+++ testing/group_analyzer.py
@@ -0,0 +1,29 @@
+import argparse
+
+from humanization.common import config_loader
+from humanization.common.utils import configure_logger
+from humanization.external_models.ablang_utils import get_attentions
+
+config = config_loader.Config()
+logger = configure_logger(config, "Group analyzer")
+
+
+def main():
+    while True:
+        seq = input("> ")
+        attentions = get_attentions(seq)
+        length = len(seq)
+        for i in range(length):
+            res_attn = sorted(enumerate(attentions[i]), key=lambda x: (x[1], x[0]))
+            ans = ["."] * length
+            for j in range(10):
+                pos = res_attn[j][0]
+                ans[pos] = str(j)
+            ans[i] = "*"
+            print(f"{i:03d}#. {ans}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='''Group analyzer of sequences''')
+    args = parser.parse_args()
+    main()
