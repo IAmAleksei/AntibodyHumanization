16e3f25f511610af520375319e6cb00e040f4578
alex-shishkin <alex-shishkin@yandex-team.ru>
2024-04-07 19:03:10 +0300

    Add attention grouping calculation

--- testing/group_analyzer.py
+++ testing/group_analyzer.py
@@ -8,17 +8,20 @@ config = config_loader.Config()
 logger = configure_logger(config, "Group analyzer")
 
 
-def main(group_size):
+def main(group_size, only_changes):
     while True:
         original_seq = input("Wild > ")
         humanized_seq = input("Hum >  ")
         attentions = get_attentions(original_seq)
         length = len(original_seq)
         print("       " + original_seq)
+        humanized_changes = [False] * length
         if humanized_seq != "":
-            humanized_changes = ['X' if humanized_seq[i] == original_seq[i] else '.' for i in range(length)]
-            print("       " + ''.join(humanized_changes))
+            humanized_changes = [humanized_seq[i] != original_seq[i] for i in range(length)]
+            print("       " + ''.join('X' if ch else '.' for ch in humanized_changes))
         for i in range(length):
+            if only_changes and not humanized_changes[i]:
+                continue
             res_attn = sorted(enumerate(attentions[i]), key=lambda x: (x[1], x[0]), reverse=True)
             ans = ["."] * length
             for j in range(group_size):
@@ -31,5 +34,6 @@ def main(group_size):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='''Group analyzer of sequences''')
     parser.add_argument('--size', type=int, required=False, default=5, help='Group size')
+    parser.add_argument('--only-changes', type=bool, required=False, default=True, help='Show only changes groups')
     args = parser.parse_args()
-    main(args.size)
+    main(args.size, args.only_changes)
