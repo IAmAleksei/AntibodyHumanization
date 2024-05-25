b9f141357a4c41b2937afda0a48bc96ce7e5b411
alex-shishkin <alex-shishkin@yandex-team.ru>
2024-05-25 17:14:01 +0300

    Add pdb generation

--- python/src/humanization/external_models/immunebuilder_utils.py
+++ python/src/humanization/external_models/immunebuilder_utils.py
@@ -1,3 +1,6 @@
+import argparse
+import os.path
+
 import numpy as np
 from ImmuneBuilder import NanoBodyBuilder2
 
@@ -16,14 +19,22 @@ def make_pdb(seq: str, pdb_path: str):
     antibody.save(pdb_path)
 
 
-def main():
-    while True:
-        sequence = input("Enter sequence (or `exit`): ")
-        if sequence.strip() == "exit":
-            break
-        pdb_path = input("Enter PDB path: ")
-        make_pdb(sequence, pdb_path)
+def main(antibody, output_path, files):
+    from Bio import SeqIO
+    for file in files:
+        for seq in SeqIO.parse(file, 'fasta'):
+            mab = seq.name.split("_")[0]
+            if mab != antibody:
+                continue
+            way = seq.name.split("_")[2]
+            prepared_seq = str(seq.seq).replace('X', '')
+            make_pdb(prepared_seq, os.path.join(output_path, f"{mab}_{way}.pdb"))
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='''Comparator of sequences''')
+    parser.add_argument('antibody', type=str, help='Name of antibody')
+    parser.add_argument('output', type=str, help='Output pdb location')
+    parser.add_argument('files', metavar='file', type=str, nargs='+', help='Name of files')
+    args = parser.parse_args()
+    main(args.antibody, args.output, args.files)
