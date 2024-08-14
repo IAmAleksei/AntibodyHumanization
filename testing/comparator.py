96270abb8c0c9c656f88a0b4218566b7b94d2884
alex-shishkin <alex-shishkin@yandex-team.ru>
2024-08-14 14:22:19 +0300

    Fixes

--- testing/comparator.py
+++ testing/comparator.py
@@ -14,8 +14,8 @@ from humanization.external_models.antiberta_utils import get_antiberta_embedding
 from humanization.external_models.biophi_utils import get_oasis_humanness
 from humanization.external_models.embedding_utils import diff_embeddings
 from humanization.external_models.immunebuilder_utils import get_immunebuilder_embedding
-from humanization.external_models.sapiens_utils import get_sapiens_embedding
-from humanization.humanness_calculator.model_wrapper import load_all_models, ModelWrapper
+# from humanization.external_models.sapiens_utils import get_sapiens_embedding
+from humanization.humanness_calculator.model_wrapper import load_all_models
 
 config = config_loader.Config()
 logger = configure_logger(config, "Comparator")
@@ -32,13 +32,15 @@ def v_gene_type(v_gene_scorer, aligned_seq: List[str]) -> ChainType:
     return ChainType.from_oas_type(v_gene_scorer.query(aligned_seq)[0][2])
 
 
-def catboost_humanness_score(models, v_gene_scorer, aligned_seq: List[str]):
-    chain_type = v_gene_type(v_gene_scorer, aligned_seq)
-    return models[chain_type].predict_proba(aligned_seq)[1]
-
-
-COLUMNS = ["Seq", "Type", "ThDist", "WDist", "HuVGS", "WVGS", "ThBerta", "WBerta", "ThAbody", "WAbody",
-           "ThSap", "WSap", "ThAblang", "WAblang", "OASId", "OASPerc", "HumCM", "MurCM", "RheCM"]
+COLUMNS = ["Seq", "Type", "ThDist", "WDist", "HuVGS", "WVGS",
+           # "ThBerta",
+           "WBerta",
+           # "ThAbody",
+           "WAbody",
+           # "ThSap",
+           # "WSap",
+           # "ThAblang",
+           "WAblang", "OASId", "OASPerc", "HumCM", "MurCM", "RheCM"]
 
 
 def print_info(seq: str, way: str, v_gene_scorer, wild_v_gene_scorer, biophi_path, models, wild_models,
@@ -47,11 +49,11 @@ def print_info(seq: str, way: str, v_gene_scorer, wild_v_gene_scorer, biophi_pat
                wild: str = None, thera: str = None):
     seq_emb_seq = get_antiberta_embedding(" ".join(seq))
     struct_emb_seq = get_immunebuilder_embedding(seq)
-    sap_emb_seq = get_sapiens_embedding(seq)
+    # sap_emb_seq = get_sapiens_embedding(seq)
     abl_emb_seq = get_ablang_embedding(seq)
     oasis_ident_seq = get_oasis_humanness(biophi_path, seq)
 
-    aligned_seq = annotate_single(seq, ChothiaHeavy(), GeneralChainType.HEAVY)
+    a_seq = annotate_single(seq, ChothiaHeavy(), GeneralChainType.HEAVY)
 
     args = [
         seq[:25] + "...",
@@ -60,20 +62,19 @@ def print_info(seq: str, way: str, v_gene_scorer, wild_v_gene_scorer, biophi_pat
         edit_distance.SequenceMatcher(seq, wild).distance() if wild is not None else "",
         round(optional_v_gene_score(v_gene_scorer, seq), 3),
         round(optional_v_gene_score(wild_v_gene_scorer, seq), 3),
-        round(diff_embeddings(seq_emb_thera, seq_emb_seq), 3) if thera is not None else "",
+        # round(diff_embeddings(seq_emb_thera, seq_emb_seq), 3) if thera is not None else "",
         round(diff_embeddings(seq_emb_wild, seq_emb_seq), 3) if wild is not None else "",
-        round(diff_embeddings(struct_emb_thera, struct_emb_seq), 4) if thera is not None else "",
+        # round(diff_embeddings(struct_emb_thera, struct_emb_seq), 4) if thera is not None else "",
         round(diff_embeddings(struct_emb_wild, struct_emb_seq), 4) if wild is not None else "",
-        round(diff_embeddings(sap_emb_thera, sap_emb_seq), 3) if thera is not None else "",
-        round(diff_embeddings(sap_emb_wild, sap_emb_seq), 3) if wild is not None else "",
-        round(diff_embeddings(abl_emb_thera, abl_emb_seq), 4) if thera is not None else "",
+        # round(diff_embeddings(sap_emb_thera, sap_emb_seq), 3) if thera is not None else "",
+        # round(diff_embeddings(sap_emb_wild, sap_emb_seq), 3) if wild is not None else "",
+        # round(diff_embeddings(abl_emb_thera, abl_emb_seq), 4) if thera is not None else "",
         round(diff_embeddings(abl_emb_wild, abl_emb_seq), 4) if wild is not None else "",
         round(oasis_ident_seq.get_oasis_identity(0.5), 2),
         round(oasis_ident_seq.get_oasis_percentile(0.5), 2),
-        round(catboost_humanness_score(models, v_gene_scorer, aligned_seq), 2) if aligned_seq is not None else "",
+        round(models[v_gene_type(v_gene_scorer, a_seq)].predict_proba([a_seq])[0, 1], 2) if a_seq is not None else "",
     ]
-
-    args.extend([round(model.predict_proba(aligned_seq)[1], 2) for model in wild_models if aligned_seq is not None])
+    args.extend([round(model.predict_proba([a_seq])[0, 1], 2) for model in wild_models if a_seq is not None])
 
     print(*args, sep=",")
 
@@ -101,7 +102,8 @@ def main(files, dataset, wild_dataset, biophi_path, human_models_dir, murine_mod
         wild = next(seq for way, seq in lst if "Wild" == way)
         seq_emb_thera, seq_emb_wild = get_antiberta_embedding(" ".join(thera)), get_antiberta_embedding(" ".join(wild))
         struct_emb_thera, struct_emb_wild = get_immunebuilder_embedding(thera), get_immunebuilder_embedding(wild)
-        sap_emb_thera, sap_emb_wild = get_sapiens_embedding(thera), get_sapiens_embedding(wild)
+        # sap_emb_thera, sap_emb_wild = get_sapiens_embedding(thera), get_sapiens_embedding(wild)
+        sap_emb_thera, sap_emb_wild = None, None
         abl_emb_thera, abl_emb_wild = get_ablang_embedding(thera), get_ablang_embedding(wild)
         print_info(wild, "Wild.", v_gene_scorer, wild_v_gene_scorer, biophi_path, models, wild_models,
                    seq_emb_thera, seq_emb_wild, struct_emb_thera, struct_emb_wild, sap_emb_thera, sap_emb_wild,
